a79f94f2f4554d3bc7acdd50e5e0176a3971b2a3
theandrew168 <andrew@shallowbrooksoftware.com>
2021-08-16 23:00:34 -0500

    Add LWJGL presets

--- project.clj
+++ project.clj
@@ -1,16 +1,96 @@
 ;; LWJGL version
 (def lwjgl-version "3.3.0-SNAPSHOT")
 
+;; TODO: consider just maintaining a matrix of module to available natives?
+;; Throw a warning during build that the JAR might not run on certain platforms?
+
+;; Everything
+#_(def lwjgl-modules
+  ["lwjgl"
+   "lwjgl-assimp"
+;   "lwjgl-bgfx"  ; no natives for: windows-arm64
+   "lwjgl-cuda"
+   "lwjgl-driftfx"
+   "lwjgl-egl"
+   "lwjgl-glfw"
+   "lwjgl-jawt"
+   "lwjgl-jemalloc"
+   "lwjgl-libdivide"
+   "lwjgl-llvm"
+   "lwjgl-lmdb"
+   "lwjgl-lz4"
+;   "lwjgl-meow"  ; no natives for: linux-arm32
+   "lwjgl-meshoptimizer"
+   "lwjgl-nanovg"
+   "lwjgl-nfd"
+   "lwjgl-nuklear"
+   "lwjgl-odbc"
+   "lwjgl-openal"
+   "lwjgl-opencl"
+   "lwjgl-opengl"
+   "lwjgl-opengles"
+;   "lwjgl-openvr"  ; no natives for: linux-arm32, linux-arm64, macos-arm64, windows-arm64
+   "lwjgl-opus"
+;   "lwjgl-ovr"  ; no natives for: linux, linux-arm32, linux-arm64, macos, macos-arm64, windows-arm64
+   "lwjgl-par"
+   "lwjgl-remotery"
+   "lwjgl-rpmalloc"
+   "lwjgl-shaderc"
+   "lwjgl-spvc"
+;   "lwjgl-sse"  ; no natives for: linux-arm32, linux-arm64, macos-arm64, windows-arm64
+   "lwjgl-stb"
+   "lwjgl-tinyexr"
+   "lwjgl-tinyfd"
+;   "lwjgl-tootle"  ; no natives for: linux-arm32, linux-arm64, macos-arm64, windows-arm64
+   "lwjgl-vma"
+   "lwjgl-vulkan"
+   "lwjgl-xxhash"
+   "lwjgl-yoga"
+   "lwjgl-zstd"])
+
+;; Getting Started
+#_(def lwjgl-modules
+  ["lwjgl"
+   "lwjgl-assimp"
+;   "lwjgl-bgfx"  ; no natives for: windows-arm64
+   "lwjgl-glfw"
+   "lwjgl-nanovg"
+   "lwjgl-nuklear"
+   "lwjgl-openal"
+   "lwjgl-opengl"
+   "lwjgl-par"
+   "lwjgl-stb"
+   "lwjgl-vulkan"])
+
 ;; Minimal OpenGL
 (def lwjgl-modules
   ["lwjgl"
+   "lwjgl-assimp"
    "lwjgl-glfw"
    "lwjgl-openal"
    "lwjgl-opengl"
    "lwjgl-stb"])
 
-;; It's safe to just include all native dependencies, but you might
-;; save some space if you know you don't need some platform(s).
+;; Minimal OpenGL ES
+#_(def lwjgl-modules
+  ["lwjgl"
+   "lwjgl-assimp"
+   "lwjgl-egl"
+   "lwjgl-glfw"
+   "lwjgl-openal"
+   "lwjgl-opengles"
+   "lwjgl-stb"])
+
+;; Minimal Vulkan
+#_(def lwjgl-modules
+  ["lwjgl"
+   "lwjgl-assimp"
+   "lwjgl-glfw"
+   "lwjgl-openal"
+   "lwjgl-stb"
+   "lwjgl-vulkan"])
+
+;; Targeted platforms / architectures
 (def lwjgl-platforms
   ["linux"
    "linux-arm32"
@@ -21,7 +101,7 @@
    "windows-x86"
    "windows-arm64"])
 
-;; These modules don't have any associated native packages.
+;; Modules without native packages
 (def lwjgl-no-natives
   #{"lwjgl-cuda"
     "lwjgl-egl"
